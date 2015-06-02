d37bcc3ab8928b1cf5da90b84fe7423c6f2ded5d
jcerovec <jcerovec@gmail.com>
2015-06-02 19:56:54 +0200

    Added sample app for Direct API

---
+++ NoCamera-sample/NoCamera-sample/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  NoCamera-sample
+//
+//  Created by Jura on 28/04/15.
+//  Copyright (c) 2015 MicroBlink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)takePhoto:(id)sender;
+
+@end
+
