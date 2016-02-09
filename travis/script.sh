d2d6152602c16f0f299dfc2e2c8074f44ff8f6bd
dino.gustin <dino.gustin@microblink.com>
2016-02-09 13:10:29 +0100

    Included pdf417 samples in travis script

--- travis/script.sh
+++ travis/script.sh
@@ -70,3 +70,31 @@ xctool -project Samples/Detector-sample/Detector-sample.xcodeproj \
 	-sdk iphonesimulator \
 	ONLY_ACTIVE_ARCH=NO \
  	clean build
+
+xctool -project Samples/pdf417-sample/pdf417-sample.xcodeproj \
+    -scheme pdf417-sample \
+    -configuration Debug \
+    -sdk iphonesimulator \
+    ONLY_ACTIVE_ARCH=NO \
+    clean build
+
+xctool -project Samples/pdf417-sample/pdf417-sample.xcodeproj \
+    -scheme pdf417-sample \
+    -configuration Release \
+    -sdk iphonesimulator \
+    ONLY_ACTIVE_ARCH=NO \
+    clean build
+
+xctool -project Samples/pdf417-sample-Swift/pdf417-sample-Swift.xcodeproj \
+    -scheme pdf417-sample-Swift \
+    -configuration Debug \
+    -sdk iphonesimulator \
+    ONLY_ACTIVE_ARCH=NO \
+    clean build
+
+xctool -project Samples/pdf417-sample-Swift/pdf417-sample-Swift.xcodeproj \
+    -scheme pdf417-sample-Swift \
+    -configuration Release \
+    -sdk iphonesimulator \
+    ONLY_ACTIVE_ARCH=NO \
+    clean build
