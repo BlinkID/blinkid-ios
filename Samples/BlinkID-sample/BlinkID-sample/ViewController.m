02eaa031be68a91112beae8ca31664dc3b2da687
Luka Zuanovic <luka@Marinas-Mac-mini.local>
2016-08-16 11:20:19 +0200

    Updated recognizer settings type.

--- Samples/BlinkID-sample/BlinkID-sample/ViewController.m
+++ Samples/BlinkID-sample/BlinkID-sample/ViewController.m
@@ -68,7 +68,7 @@
 
         // To specify we want to perform MRTD (machine readable travel document) recognition, initialize the MRTD recognizer settings
         PPMrtdRecognizerSettings *mrtdRecognizerSettings = [[PPMrtdRecognizerSettings alloc] init];
-
+        
         /** You can modify the properties of mrtdRecognizerSettings to suit your use-case */
 
         // Add MRTD Recognizer setting to a list of used recognizer settings
