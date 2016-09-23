2ec87aef0d963239b61e696e1c6781d303456074
dino.gustin <dino.gustin@microblink.com>
2016-09-23 11:33:18 +0200

    updated travis to xcode 8

--- travis/script.sh
+++ travis/script.sh
@@ -1,112 +1,112 @@
 #!/bin/sh
 set -e
 
-xctool -project Samples/DirectAPI-sample/DirectAPI-sample.xcodeproj \
+xcodebuild -project Samples/DirectAPI-sample/DirectAPI-sample.xcodeproj \
     -scheme DirectAPI-Sample \
     -configuration Debug \
     -sdk iphonesimulator \
     ONLY_ACTIVE_ARCH=NO \
     clean build
 
-xctool -project Samples/DirectAPI-sample/DirectAPI-sample.xcodeproj \
+xcodebuild -project Samples/DirectAPI-sample/DirectAPI-sample.xcodeproj \
     -scheme DirectAPI-Sample \
     -configuration Release \
     -sdk iphonesimulator \
     ONLY_ACTIVE_ARCH=NO \
     clean build
 
-xctool -project Samples/Templating-Sample/Templating-Sample.xcodeproj \
+xcodebuild -project Samples/Templating-Sample/Templating-Sample.xcodeproj \
     -scheme Templating-Sample \
     -configuration Debug \
     -sdk iphonesimulator \
     ONLY_ACTIVE_ARCH=NO \
     clean build
 
-xctool -project Samples/Templating-Sample/Templating-Sample.xcodeproj \
+xcodebuild -project Samples/Templating-Sample/Templating-Sample.xcodeproj \
     -scheme Templating-Sample \
     -configuration Release \
     -sdk iphonesimulator \
     ONLY_ACTIVE_ARCH=NO \
     clean build
 
-xctool -project Samples/BlinkOCR-sample/BlinkOCR-sample.xcodeproj \
+xcodebuild -project Samples/BlinkOCR-sample/BlinkOCR-sample.xcodeproj \
 	-scheme BlinkOCR-sample \
 	-configuration Debug \
 	-sdk iphonesimulator \
 	ONLY_ACTIVE_ARCH=NO \
  	clean build
 
-xctool -project Samples/BlinkOCR-sample/BlinkOCR-sample.xcodeproj \
+xcodebuild -project Samples/BlinkOCR-sample/BlinkOCR-sample.xcodeproj \
 	-scheme BlinkOCR-sample \
 	-configuration Release \
 	-sdk iphonesimulator \
 	ONLY_ACTIVE_ARCH=NO \
  	clean build
 
-xctool -project Samples/BlinkID-sample/BlinkID-sample.xcodeproj \
+xcodebuild -project Samples/BlinkID-sample/BlinkID-sample.xcodeproj \
 	-scheme BlinkID-sample \
 	-configuration Debug \
 	-sdk iphonesimulator \
 	ONLY_ACTIVE_ARCH=NO \
  	clean build
 
-xctool -project Samples/BlinkID-sample/BlinkID-sample.xcodeproj \
+xcodebuild -project Samples/BlinkID-sample/BlinkID-sample.xcodeproj \
 	-scheme BlinkID-sample \
 	-configuration Release \
 	-sdk iphonesimulator \
 	ONLY_ACTIVE_ARCH=NO \
  	clean build
 
-xctool -project Samples/BlinkID-sample-Swift/BlinkID-sample-Swift.xcodeproj \
+xcodebuild -project Samples/BlinkID-sample-Swift/BlinkID-sample-Swift.xcodeproj \
 	-scheme BlinkID-sample-Swift \
 	-configuration Debug \
 	-sdk iphonesimulator \
 	ONLY_ACTIVE_ARCH=NO \
  	clean build
 
-xctool -project Samples/BlinkID-sample-Swift/BlinkID-sample-Swift.xcodeproj \
+xcodebuild -project Samples/BlinkID-sample-Swift/BlinkID-sample-Swift.xcodeproj \
 	-scheme BlinkID-sample-Swift \
 	-configuration Release \
 	-sdk iphonesimulator \
 	ONLY_ACTIVE_ARCH=NO \
  	clean build
 
-xctool -project Samples/Detector-sample/Detector-sample.xcodeproj \
+xcodebuild -project Samples/Detector-sample/Detector-sample.xcodeproj \
 	-scheme Detector-sample \
 	-configuration Debug \
 	-sdk iphonesimulator \
 	ONLY_ACTIVE_ARCH=NO \
  	clean build
 
-xctool -project Samples/Detector-sample/Detector-sample.xcodeproj \
+xcodebuild -project Samples/Detector-sample/Detector-sample.xcodeproj \
 	-scheme Detector-sample \
 	-configuration Release \
 	-sdk iphonesimulator \
 	ONLY_ACTIVE_ARCH=NO \
  	clean build
 
-xctool -project Samples/pdf417-sample/pdf417-sample.xcodeproj \
+xcodebuild -project Samples/pdf417-sample/pdf417-sample.xcodeproj \
     -scheme pdf417-sample \
     -configuration Debug \
     -sdk iphonesimulator \
     ONLY_ACTIVE_ARCH=NO \
     clean build
 
-xctool -project Samples/pdf417-sample/pdf417-sample.xcodeproj \
+xcodebuild -project Samples/pdf417-sample/pdf417-sample.xcodeproj \
     -scheme pdf417-sample \
     -configuration Release \
     -sdk iphonesimulator \
     ONLY_ACTIVE_ARCH=NO \
     clean build
 
-xctool -project Samples/pdf417-sample-Swift/pdf417-sample-Swift.xcodeproj \
+xcodebuild -project Samples/pdf417-sample-Swift/pdf417-sample-Swift.xcodeproj \
     -scheme pdf417-sample-Swift \
     -configuration Debug \
     -sdk iphonesimulator \
     ONLY_ACTIVE_ARCH=NO \
     clean build
 
-xctool -project Samples/pdf417-sample-Swift/pdf417-sample-Swift.xcodeproj \
+xcodebuild -project Samples/pdf417-sample-Swift/pdf417-sample-Swift.xcodeproj \
     -scheme pdf417-sample-Swift \
     -configuration Release \
     -sdk iphonesimulator \
