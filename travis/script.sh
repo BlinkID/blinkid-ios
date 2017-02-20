b34937d8c3e498d64badae2b69c12e2828fe0a59
Jurica Cerovec <jurica.cerovec@microblink.com>
2017-02-20 09:09:33 +0100

    Updated travis with removed project file

--- travis/script.sh
+++ travis/script.sh
@@ -98,17 +98,3 @@ xcodebuild -project Samples/pdf417-sample/pdf417-sample.xcodeproj \
     -sdk iphonesimulator \
     ONLY_ACTIVE_ARCH=NO \
     clean build
-
-xcodebuild -project Samples/pdf417-sample-Swift/pdf417-sample-Swift.xcodeproj \
-    -scheme pdf417-sample-Swift \
-    -configuration Debug \
-    -sdk iphonesimulator \
-    ONLY_ACTIVE_ARCH=NO \
-    clean build
-
-xcodebuild -project Samples/pdf417-sample-Swift/pdf417-sample-Swift.xcodeproj \
-    -scheme pdf417-sample-Swift \
-    -configuration Release \
-    -sdk iphonesimulator \
-    ONLY_ACTIVE_ARCH=NO \
-    clean build
