016327690f086637b72d970aaf4ecd6861eea951
cularj <jure.cular@microblink.com>
2017-03-30 11:55:32 +0200

    Updated to support third type of card

--- Samples/Templating-Sample/Templating-Sample/StarbucksCardRecognizerSettings.m
+++ Samples/Templating-Sample/Templating-Sample/StarbucksCardRecognizerSettings.m
@@ -10,13 +10,18 @@
 
 static NSString *const CARD_NUMBER = @"CardNumber";
 static NSString *const CARD_SECURITY_CODE = @"SecurityCode";
+
 static NSString *const CARD_NUMBER_FIRST_TYPE = @"CardNumberFirstType";
-static NSString *const CARD_SECURITY_CODE_FIRST_TYPE = @"SecurityCodeFirstType";
 static NSString *const CARD_NUMBER_SECOND_TYPE = @"CardNumberSecondType";
+static NSString *const CARD_NUMBER_THIRD_TYPE = @"CardNumberThirdType";
+
+static NSString *const CARD_SECURITY_CODE_FIRST_TYPE = @"SecurityCodeFirstType";
 static NSString *const CARD_SECURITY_CODE_SECOND_TYPE = @"SecurityCodeSecondType";
+static NSString *const CARD_SECURITY_CODE_THIRD_TYPE = @"SecurityCodeThirdType";
 
 static NSString *const FIRST_TYPE = @"FirstType";
 static NSString *const SECOND_TYPE = @"SecondType";
+static NSString *const THIRD_TYPE = @"ThirdType";
 
 @interface StarbucksCardRecognizerSettings () <PPDocumentClassifier>
 
@@ -41,28 +46,25 @@ static NSString *const SECOND_TYPE = @"SecondType";
 - (instancetype)init {
     self = [super init];
     if (self) {
-        NSMutableArray<PPDecodingInfo *> *firstCardDecodingInfoArray = [NSMutableArray array];
-        NSMutableArray<PPDecodingInfo *> *secondCardDecodingInfoArray = [NSMutableArray array];
 
+        NSDictionary *decodingInfoArrayDictionary =
+            @{FIRST_TYPE : [NSMutableArray array], SECOND_TYPE : [NSMutableArray array], THIRD_TYPE : [NSMutableArray array]};
+        
         NSMutableArray<PPDecodingInfo *> *classificationDecodingInfoArray = [NSMutableArray array];
 
         /** Setup card number decoding */
-        [self setCardNumberWithFirstDecodingInfoArray:firstCardDecodingInfoArray
-                       andSecondCardDecodingInfoArray:secondCardDecodingInfoArray
-                   andClassificationDecodingInfoArray:classificationDecodingInfoArray];
-
+        [self setCardNumberWithDictionaryOfDecodingInfoArray:decodingInfoArrayDictionary];
+        
         /** Setup security number decoding */
-        [self setSecurityNumberWithFirstDecodingInfoArray:firstCardDecodingInfoArray
-                           andSecondCardDecodingInfoArray:secondCardDecodingInfoArray
-                       andClassificationDecodingInfoArray:classificationDecodingInfoArray];
+        [self setSecurityNumberWithClassificationDecodingInfoArray:classificationDecodingInfoArray];
 
         /**
-         * Create ID card document specification. Document specification defines geometric/scanning properties of documents to be detected
+         * Create star bucks card document specification. Document specification defines geometric/scanning properties of documents to be detected
          */
         PPDocumentSpecification *idSpec = [PPDocumentSpecification newFromPreset:PPDocumentPresetId1Card];
 
         /**
-         * Set decoding infos as our classification decoding infos. One has location of document number on old id, other on new Id
+         * Set decoding infos as our classification decoding infos. Each type of card has different location of security number
          */
         [idSpec setDecodingInfo:classificationDecodingInfoArray];
 
@@ -84,27 +86,30 @@ static NSString *const SECOND_TYPE = @"SecondType";
          * Add decoding infos for classifier results. These infos and their parsers will only be processed if classifier outputs the
          * selected result
          */
-        [self setDecodingInfoSet:firstCardDecodingInfoArray forClassifierResult:FIRST_TYPE];
-        [self setDecodingInfoSet:secondCardDecodingInfoArray forClassifierResult:SECOND_TYPE];
+        
+        for (NSString *key in decodingInfoArrayDictionary) {
+            [self setDecodingInfoSet:decodingInfoArrayDictionary[key] forClassifierResult:key];
+        }
     }
     return self;
 }
 
 #pragma mark - Decoding info
 
-- (void)setCardNumberWithFirstDecodingInfoArray:(NSMutableArray<PPDecodingInfo *> *)firstCardDecodingInfoArray
-                 andSecondCardDecodingInfoArray:(NSMutableArray<PPDecodingInfo *> *)secondCardDecodingInfoArray
-             andClassificationDecodingInfoArray:(NSMutableArray<PPDecodingInfo *> *)classificationDecodingInfoArray {
+- (void)setCardNumberWithDictionaryOfDecodingInfoArray:(NSDictionary *)decodingInfoArrayDictionary {
 
     int dewarpedHeight = 100;
+    
+    CGRect firstLocation = CGRectMake(0.2328f, 0.1253f, 0.5953f, 0.0927f);
+    CGRect secondLocation = CGRectMake(0.2088f, 0.1688f, 0.5500f, 0.0866f);
+    CGRect thirdLocation = CGRectMake(0.2225f, 0.6554f, 0.4812f, 0.0871f);
 
-    CGRect firstLocation = CGRectMake(0.3689f, 0.1480f, 0.4531f, 0.0561f);
-    CGRect secondLocation = CGRectMake(0.2198f, 0.1732f, 0.4148f, 0.0736f);
-
-    [firstCardDecodingInfoArray
+    [decodingInfoArrayDictionary[FIRST_TYPE]
         addObject:[[PPDecodingInfo alloc] initWithLocation:firstLocation dewarpedHeight:dewarpedHeight uniqueId:CARD_NUMBER_FIRST_TYPE]];
-    [secondCardDecodingInfoArray
-        addObject:[[PPDecodingInfo alloc] initWithLocation:secondLocation dewarpedHeight:dewarpedHeight uniqueId:CARD_NUMBER_SECOND_TYPE]];
+    [decodingInfoArrayDictionary[SECOND_TYPE]
+     addObject:[[PPDecodingInfo alloc] initWithLocation:secondLocation dewarpedHeight:dewarpedHeight uniqueId:CARD_NUMBER_SECOND_TYPE]];
+    [decodingInfoArrayDictionary[THIRD_TYPE]
+     addObject:[[PPDecodingInfo alloc] initWithLocation:thirdLocation dewarpedHeight:dewarpedHeight uniqueId:CARD_NUMBER_THIRD_TYPE]];
 
     PPRegexOcrParserFactory *documentNumberParser = [[PPRegexOcrParserFactory alloc] initWithRegex:@"(\\d{4} ?){4}"];
 
@@ -118,18 +123,20 @@ static NSString *const SECOND_TYPE = @"SecondType";
     options.minimalLineHeight = 30;
     [documentNumberParser setOptions:options];
 
+   
+    
     [self addOcrParser:documentNumberParser name:CARD_NUMBER group:CARD_NUMBER_FIRST_TYPE];
     [self addOcrParser:documentNumberParser name:CARD_NUMBER group:CARD_NUMBER_SECOND_TYPE];
+    [self addOcrParser:documentNumberParser name:CARD_NUMBER group:CARD_NUMBER_THIRD_TYPE];
 }
 
-- (void)setSecurityNumberWithFirstDecodingInfoArray:(NSMutableArray<PPDecodingInfo *> *)firstCardDecodingInfoArray
-                     andSecondCardDecodingInfoArray:(NSMutableArray<PPDecodingInfo *> *)secondCardDecodingInfoArray
-                 andClassificationDecodingInfoArray:(NSMutableArray<PPDecodingInfo *> *)classificationDecodingInfoArray {
+- (void)setSecurityNumberWithClassificationDecodingInfoArray:(NSMutableArray<PPDecodingInfo *> *)classificationDecodingInfoArray {
 
     int dewarpedHeight = 100;
 
-    CGRect firstLocation = CGRectMake(0.5016f, 0.2194f, 0.1812f, 0.0612f);
-    CGRect secondLocation = CGRectMake(0.7280f, 0.1732f, 0.1978f, 0.0736f);
+    CGRect firstLocation = CGRectMake(0.4953f, 0.2180f, 0.2266f, 0.0677f);
+    CGRect secondLocation = CGRectMake(0.7198f, 0.1602f, 0.2253f, 0.1082f);
+    CGRect thirdLocation = CGRectMake(0.7238f, 0.6436f, 0.2338f, 0.0851f);
 
     [classificationDecodingInfoArray addObject:[[PPDecodingInfo alloc] initWithLocation:firstLocation
                                                                          dewarpedHeight:dewarpedHeight
@@ -138,6 +145,10 @@ static NSString *const SECOND_TYPE = @"SecondType";
     [classificationDecodingInfoArray addObject:[[PPDecodingInfo alloc] initWithLocation:secondLocation
                                                                          dewarpedHeight:dewarpedHeight
                                                                                uniqueId:CARD_SECURITY_CODE_SECOND_TYPE]];
+    
+    [classificationDecodingInfoArray addObject:[[PPDecodingInfo alloc] initWithLocation:thirdLocation
+                                                                         dewarpedHeight:dewarpedHeight
+                                                                               uniqueId:CARD_SECURITY_CODE_THIRD_TYPE]];
 
     PPRegexOcrParserFactory *documentNumberParser = [[PPRegexOcrParserFactory alloc] initWithRegex:@"\\d{8}"];
 
@@ -153,6 +164,7 @@ static NSString *const SECOND_TYPE = @"SecondType";
 
     [self addOcrParser:documentNumberParser name:CARD_SECURITY_CODE group:CARD_SECURITY_CODE_FIRST_TYPE];
     [self addOcrParser:documentNumberParser name:CARD_SECURITY_CODE group:CARD_SECURITY_CODE_SECOND_TYPE];
+    [self addOcrParser:documentNumberParser name:CARD_SECURITY_CODE group:CARD_SECURITY_CODE_THIRD_TYPE];
 }
 
 - (NSString *)extractMessageFromResult:(PPBlinkOcrRecognizerResult *)result {
@@ -168,26 +180,31 @@ static NSString *const SECOND_TYPE = @"SecondType";
 #pragma mark - PPDocumentClassifier
 
 - (NSString *)classifyDocumentFromResult:(PPTemplatingRecognizerResult *)result {
-    /**
-     * Get the result of parsing the location of document number on old ID.
-     */
     _type = @"";
-    NSString *documentNumber = [result parsedResultForName:CARD_SECURITY_CODE parserGroup:CARD_SECURITY_CODE_FIRST_TYPE];
-    if (documentNumber != nil && ![documentNumber isEqualToString:@""]) {
-        // If result exists then we are dealing with old ID
+    NSString *securityNumber =  [result parsedResultForName:CARD_SECURITY_CODE parserGroup:CARD_SECURITY_CODE_THIRD_TYPE];
+    
+    if (securityNumber != nil && ![securityNumber isEqualToString:@""] ){
+        // If result exists then we are dealing with third type card
+        _type = THIRD_TYPE;
+        return _type;
+    }
+    
+    securityNumber = [result parsedResultForName:CARD_SECURITY_CODE parserGroup:CARD_SECURITY_CODE_FIRST_TYPE];
+    
+    if (securityNumber != nil && ![securityNumber isEqualToString:@""] ){
+        // If result exists then we are dealing with second type card
         _type = FIRST_TYPE;
+        return _type;
     }
-    /**
-     * Get the result of parsing the location of document number on new ID.
-     */
-    documentNumber = [result parsedResultForName:CARD_SECURITY_CODE parserGroup:CARD_SECURITY_CODE_SECOND_TYPE];
-    if (documentNumber != nil && ![documentNumber isEqualToString:@""]) {
-        // If result exists then we are dealing with new ID
+    
+    securityNumber = [result parsedResultForName:CARD_SECURITY_CODE parserGroup:CARD_SECURITY_CODE_SECOND_TYPE];
+
+    if (securityNumber != nil && ![securityNumber isEqualToString:@""] ){
+        // If result exists then we are dealing with first type card
         _type = SECOND_TYPE;
+        return _type;
     }
-    /**
-     * Document is detected but it doesnt contain document numbers on their expected locations
-     */
+    
     return _type;
 }
 
