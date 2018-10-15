727a7f6cbbd243a0fa2df9849fd3c36869f9649e
juraskrlec <jura.skrlec@microblink.com>
2018-10-15 17:57:04 +0200

    Updated for deployment sdk 12.0

--- Samples/pdf417-sample-Swift/pdf417-sample-Swift/AppDelegate.swift
+++ Samples/pdf417-sample-Swift/pdf417-sample-Swift/AppDelegate.swift
@@ -13,10 +13,8 @@ import MicroBlink
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-    private func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         return true
     }
 
