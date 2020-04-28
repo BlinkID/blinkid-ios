f45f8aa9aa0cc413d640444a61878185f7bbdf0b
Jura Skrlec <jura.skrlec@microblink.com>
2020-04-28 14:51:48 +0200

    Updated license key for v5.4.0

--- Samples/BlinkID-sample-Swift/BlinkID-sample-Swift/ViewController.swift
+++ Samples/BlinkID-sample-Swift/BlinkID-sample-Swift/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Valid until: 2020-06-26
+        // Valid until: 2020-08-06
         MBMicroblinkSDK.sharedInstance().setLicenseResource("blinkid-license", withExtension: "txt", inSubdirectory: "", for: Bundle.main)
     }
 
