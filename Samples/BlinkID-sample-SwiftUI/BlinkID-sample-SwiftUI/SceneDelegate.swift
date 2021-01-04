bc33f13ea9e33e321db218b6ec5c080ebb00e841
Jura Skrlec <jura.skrlec@microblink.com>
2021-01-04 15:32:42 +0100

    SDK-98: Add SwiftUI Sample

---
+++ Samples/BlinkID-sample-SwiftUI/BlinkID-sample-SwiftUI/SceneDelegate.swift
@@ -0,0 +1,62 @@
+// Created by Jura Skrlec on 23/12/2020.
+// Copyright (c) 2020 Microblink Ltd. All rights reserved.
+
+// ANY UNAUTHORIZED USE OR SALE, DUPLICATION, OR DISTRIBUTION
+// OF THIS PROGRAM OR ANY OF ITS PARTS, IN SOURCE OR BINARY FORMS,
+// WITH OR WITHOUT MODIFICATION, WITH THE PURPOSE OF ACQUIRING
+// UNLAWFUL MATERIAL OR ANY OTHER BENEFIT IS PROHIBITED!
+// THIS PROGRAM IS PROTECTED BY COPYRIGHT LAWS AND YOU MAY NOT
+// REVERSE ENGINEER, DECOMPILE, OR DISASSEMBLE IT.
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+
+        // Create the SwiftUI view that provides the window contents.
+        let contentView = ContentView()
+
+        // Use a UIHostingController as window root view controller.
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+    }
+}
