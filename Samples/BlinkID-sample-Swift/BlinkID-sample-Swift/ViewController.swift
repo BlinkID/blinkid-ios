b73895eca8bea67bd23289b19ec2c0c39c505474
Mijo Gracanin <mijo.gracanin@gmail.com>
2022-07-22 12:29:56 +0200

    Update samples and license for v5.18.0

--- Samples/BlinkID-sample-Swift/BlinkID-sample-Swift/ViewController.swift
+++ Samples/BlinkID-sample-Swift/BlinkID-sample-Swift/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Valid until: 2022-10-19
+        // Valid until: 2022-11-19
         MBMicroblinkSDK.shared().setLicenseResource("blinkid-license", withExtension: "txt", inSubdirectory: "", for: Bundle.main) { (_) in
         }
     }
--- Samples/DirectAPI-sample-Swift/DirectAPI-sample-Swift/ViewController.swift
+++ Samples/DirectAPI-sample-Swift/DirectAPI-sample-Swift/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Valid until: 2022-10-19
+        // Valid until: 2022-11-19
         MBMicroblinkSDK.shared().setLicenseResource("blinkid-license", withExtension: "txt", inSubdirectory: "", for: Bundle.main) { (_) in
         }
         
