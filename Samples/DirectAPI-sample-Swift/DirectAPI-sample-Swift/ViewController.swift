c0fac6a6a73aaa980e474ca1f8fd427c98b1959d
Toni Kreso <tonikreso2105@gmail.com>
2022-10-10 13:08:40 +0200

    Update samples and license for v5.20.0

--- Samples/DirectAPI-sample-Swift/DirectAPI-sample-Swift/ViewController.swift
+++ Samples/DirectAPI-sample-Swift/DirectAPI-sample-Swift/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Valid until: 2023-2-5
+        // Valid until: 2023-2-10
         MBMicroblinkSDK.shared().setLicenseResource("blinkid-license", withExtension: "txt", inSubdirectory: "", for: Bundle.main) { (_) in
         }
         
