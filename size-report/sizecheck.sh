6898242e6594bcf82e11b0fc061cfa6cf597ddab
Mijo Gracanin <mijo.gracanin@gmail.com>
2022-11-28 15:34:00 +0100

    Fix code quality issues

--- size-report/sizecheck.sh
+++ size-report/sizecheck.sh
@@ -14,15 +14,15 @@ PATH_TO_SIZE_REPORT=$PATH_TO_SAMPLES/../size-report
 
 if [ -d "$PATH_TO_SAMPLES" ]; then
 
-  if [ $PROJECT_NAME == "Pdf417Mobi" ]; then
+  if [ "$PROJECT_NAME" == "Pdf417Mobi" ]; then
     PROJECT_NAME = "pdf417"
   fi
 
-  xcodebuild -project $PATH_TO_SAMPLES/$PROJECT_NAME-sample-Swift/$PROJECT_NAME-sample-Swift.xcodeproj -sdk iphoneos archive -archivePath $PATH_TO_SIZE_REPORT/app.xcarchive -scheme $PROJECT_NAME-sample-Swift
+  xcodebuild -project "$PATH_TO_SAMPLES/$PROJECT_NAME-sample-Swift/$PROJECT_NAME-sample-Swift.xcodeproj" -sdk iphoneos archive -archivePath "$PATH_TO_SIZE_REPORT/app.xcarchive" -scheme "$PROJECT_NAME-sample-Swift"
 
-  xcodebuild -exportArchive -archivePath $PATH_TO_SIZE_REPORT/app.xcarchive -exportPath $PATH_TO_SIZE_REPORT/app.ipa -exportOptionsPlist $PATH_TO_SIZE_REPORT/exportOptions.plist -allowProvisioningUpdates
+  xcodebuild -exportArchive -archivePath "$PATH_TO_SIZE_REPORT/app.xcarchive" -exportPath "$PATH_TO_SIZE_REPORT/app.ipa" -exportOptionsPlist "$PATH_TO_SIZE_REPORT/exportOptions.plist" -allowProvisioningUpdates
 
   cp "$PATH_TO_SIZE_REPORT/app.ipa/App Thinning Size Report.txt" "$PATH_TO_SIZE_REPORT"
   
-  rm -r $PATH_TO_SIZE_REPORT/app.xcarchive $PATH_TO_SIZE_REPORT/app.ipa
+  rm -r "$PATH_TO_SIZE_REPORT/app.xcarchive" "$PATH_TO_SIZE_REPORT/app.ipa"
 fi
