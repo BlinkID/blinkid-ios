a8222ced852a298cd7d014ff8f30618e8c128a6f
Mijo Gracanin <mijo.gracanin@gmail.com>
2022-12-15 22:49:32 +0100

    Update for v6

--- Package.swift
+++ Package.swift
@@ -9,14 +9,14 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BlinkID",
-            targets: ["Microblink"]
+            targets: ["BlinkID"]
             )
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
-            name: "Microblink",
-            url: "https://github.com/BlinkID/blinkid-ios/releases/download/v5.20.1/Microblink.xcframework.zip",
+            name: "BlinkID",
+            url: "https://github.com/BlinkID/blinkid-ios/releases/download/v6.0.0/BlinkID.xcframework.zip",
             checksum: "7a39c387890934f64c2cc844db1582396f06c062ff886d58e32d4b40fc44ede6")
     ]
 )
