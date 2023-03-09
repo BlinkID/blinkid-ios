1237e3fc9bfd0429834bc13cc42d9b1429be4f49
Toni Kreso <tonikreso2105@gmail.com>
2023-03-09 12:23:05 +0100

    Update samples and licence for v6.1.2

--- Samples/BlinkID-sample-SwiftUI/BlinkID-sample-SwiftUI/AppDelegate.swift
+++ Samples/BlinkID-sample-SwiftUI/BlinkID-sample-SwiftUI/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        // Valid until: 2023-6-17
+        // Valid until: 2023-7-9
         MBMicroblinkSDK.shared().setLicenseResource("blinkid-license", withExtension: "txt", inSubdirectory: nil, for: .main) { (_) in
         }
         return true
